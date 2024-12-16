6062ea8d0acd2eaa9f6dd334c0a2d73f73de4189
bradyp34 <brady.petterson@gmail.com>
2024-12-16 02:14:09 -0600

    created project for unit tests and set up environment

---
+++ BradyPettersonDeveloperWebsite.Tests/NavMenuTests.cs
@@ -0,0 +1,22 @@
+using Bunit;
+using Xunit;
+using BradyPettersonDeveloperWebsite.Components;
+using BradyPettersonDeveloperWebsite.Components.Layout;
+
+public class NavMenuTests : TestContext {
+    //create unit tests here
+
+/*
+    [Fact]
+    public void NavMenuRendersCorrectly () {
+        // Arrange & Act
+        var component = RenderComponent<NavMenu>();
+
+        // Assert
+        component.MarkupMatches(@"
+            <div class=""sidebar"">
+                <!-- Expected HTML structure here -->
+            </div>
+        ");
+    }*/
+}
